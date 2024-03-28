4e15e96cbb1d7003e86ac24d15296cc4340d4150
Nathaniel <me@nathanielbennett.com>
2024-03-28 14:03:11 -0400

    Fix unresolved import

--- rscap/src/linux/prelude.rs
+++ rscap/src/linux/prelude.rs
@@ -1,5 +1,6 @@
-pub use super::addr::{Interface, L2Addr, L2AddrAny, L2AddrIp, L2AddrUnspec};
+pub use super::addr::{L2Addr, L2AddrAny, L2AddrIp, L2AddrUnspec};
 pub use super::l2::{
     FanoutAlgorithm, L2MappedSocket, L2RxMappedSocket, L2Socket, L2TxMappedSocket,
 };
 pub use super::{PacketStatistics, RxTimestamping, TxTimestamping};
+pub use crate::Interface;
