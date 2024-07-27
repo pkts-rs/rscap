6bf72948a5ae03ca962731eb5384941e84963a0e
Nathaniel <me@nathanielbennett.com>
2024-07-26 18:08:59 -0600

    Add metadata to pkts-common cargo; remove TODO

--- pkts-common/src/lib.rs
+++ pkts-common/src/lib.rs
@@ -8,8 +8,6 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// TODO: make buffer a Buffer<'a> instead?
-
 #![forbid(unsafe_code)]
 
 use core::array;
