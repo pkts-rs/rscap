3174e264ad9adea96ddd24883c87a85cff997b72
Nathaniel Bennett <me@nathanielbennett.com>
2024-11-02 17:52:03 -0400

    Add SPDX identifier to source files

--- rscap/src/npcap/adapter.rs
+++ rscap/src/npcap/adapter.rs
@@ -1,3 +1,13 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+//
+// Copyright (c) 2024 Nathaniel Bennett <me[at]nathanielbennett[dotcom]>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::ffi::CStr;
 use std::io;
 use std::mem::MaybeUninit;
